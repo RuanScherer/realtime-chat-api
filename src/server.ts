96febc61d84451c69a8f63bffcafbd0df8cb8fa9
Ruan Scherer <ruan.vscherer@gmail.com>
2020-07-09 16:00:19 -0300

    Emit a event for new messages

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ var io = require('socket.io')(http);
 
 io.on('connection', (socket: Socket) => {
 	socket.emit("loadOldMessages")
+	socket.on('newMessage', message => {
+		socket.broadcast.emit('loadNewMessage', message)
+	})
 });
 
 http.listen(3333, () => console.log("App listening on port 3333"))
