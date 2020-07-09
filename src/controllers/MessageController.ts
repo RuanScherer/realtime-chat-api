cd8649b87884618046160199f77c69c7c10d4d67
Ruan Scherer <ruan.vscherer@gmail.com>
2020-07-09 13:49:00 -0300

    Return all messages

--- src/controllers/MessageController.ts
+++ src/controllers/MessageController.ts
@@ -2,12 +2,22 @@ import { Request, Response } from 'express'
 import Message from '../models/Message'
 
 class MessageController {
+	public async index(request: Request, response: Response) {
+		const messages = await Message.find()
+
+		if (!messages) return response.status(500).send()
+
+		return response.send({ messages })
+	}
+
 	public async store(request: Request, response: Response) {
 		const { username, content } = request.body
 
-		if (!username || !content) return response.send(400)
+		if (!username || !content) return response.status(400).send()
 
 		const message = await Message.create({ username, content })
+
+		if (!message) return response.status(500).send()
 
 		return response.send({ message })
 	}
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import MessageController from './controllers/MessageController'
 
 const router = express.Router()
 
+router.get('/messages', MessageController.index)
 router.post('/messages', MessageController.store)
 
 export default router
