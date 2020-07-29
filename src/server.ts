fc5f2cab5c51a4c1b986af8bbc22e020e0f81346
Ruan Scherer <ruan.vscherer@gmail.com>
2020-07-29 12:45:25 -0300

    Updating for migrate OS

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ io.on('connection', (socket: ISocket) => {
 	socket.emit("loadOldMessages")
 
 	socket.on('newMessage', data => {
-		socket.broadcast.emit('loadNewMessage', data)
+		io.emit('loadNewMessage', data)
 	})
 })
 
